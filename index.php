daf16ab8854686ee664e6bf8fe383bbc580f35d0
GlennMoreno <lolglenn26@gmail.com>
2025-01-07 08:59:08 +0800

    Menampilkan nama dan nisn

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+    //create variable
+    $nama ='Reinhard Glenn heavenjoy Turangan';
+    $nisn ='0079119090';
 
+    //get variable
+    echo 'Nama : ', $nama;
+    echo '<br>';
+    echo 'Nisn : ',$nisn;
 ?>
